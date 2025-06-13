7c052642cc626eef4d5943abd5a9a102cafc1bbe
Alvaro <alvaro.rodriguez27@educa.madrid.org>
2025-06-13 16:50:58 +0200

    13/06/25 README.md

--- servicio/src/main/resources/data.sql
+++ servicio/src/main/resources/data.sql
@@ -6,8 +6,3 @@ ALTER TABLE card CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE character_card CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE item_card CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE board_card CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- ---------------------------------
--- Cartas base (tabla `card`)
--- columnas: id, name, description, image, cost, rarity, type
--- ---------------------------------
